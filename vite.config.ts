9a7ea363da700df497cd566dbe4ccf99f0878103
Bohdan <snizinkavolshebna@gmail.com>
2024-04-15 03:12:09 +0300

    Sass options setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+              @import "./src/assets/styles/main.scss";
+            `
+      }
+    }
+  },
   plugins: [
     vue(),
   ],
